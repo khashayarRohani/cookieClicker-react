8e6a2968e86d61c747698b4c6247fdb0b98086fd
khashayarRohani <khashayarrhi@gmail.com>
2024-08-09 20:59:41 +0100

    css for score buttons

--- src/components/LevelButtons.jsx
+++ src/components/LevelButtons.jsx
@@ -1,19 +1,30 @@
 export default function LevelButtons(props) {
   console.log(props.currentLevel);
+
+  const imageStyle = {
+    transition: "transform 0.2s ease",
+    animation: "moveLeftRight 20s ease-in-out infinite", // Default animation
+  };
+
   return (
     <div className="ScoreButtons">
       {props.lev.slice(1).map((level) => (
         <div key={level.id} style={{ position: "relative" }}>
           <button
+            style={{ position: "relative" }}
             onClick={() => props.buyLevel(level.id)} // Inline function to pass level.id
             disabled={
               props.cookies < level.cost || level.id <= props.currentLevel
             }
             className={props.currentLevel === level.id ? "Selected" : ""}
           >
+            <img
+              className="ScoreImage"
+              style={props.currentLevel === level.id ? imageStyle : null}
+              src={level.src}
+            />
             {level.name}
           </button>
-          <img className="ScoreImage" src={level.src} />
         </div>
       ))}
     </div>
